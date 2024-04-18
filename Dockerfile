3a4ccf655bb361c73d4ba2a00e8d0a6cd7474c9e
Ikhsan N. Rosyidin <ikhsan.rosyidin@traveloka.com>
2024-04-18 11:52:57 +0700

    build with https://aws.plainenglish.io/deploying-a-docker-container-in-aws-using-fargate-5a19a140b018
    Signed-off-by: Ikhsan N. Rosyidin <ikhsan.rosyidin@traveloka.com>

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.12-slim
-ADD . /app
 WORKDIR /app
+COPY . .
 RUN pip install --no-cache-dir -r requirements.txt
-EXPOSE 5000
-CMD ["flask", "run", "--host=0.0.0.0"]
+EXPOSE 8080
+CMD ["gunicorn", "--bind", "0.0.0.0:8080", "app:app"]
--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/healthcheck')
+def healthcheck():
+    return "OK"
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -10,3 +15,7 @@ def index():
 @app.route('/polls')
 def polls():
     return render_template('polls.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=8080)
