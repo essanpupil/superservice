551309b4101716f967956cb42744156aa960c99b
Ikhsan N. Rosyidin <ikhsan.rosyidin@traveloka.com>
2024-04-22 15:52:45 +0700

    update database log
    Signed-off-by: Ikhsan N. Rosyidin <ikhsan.rosyidin@traveloka.com>

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template
 import mysql.connector
+import logging
 
 app = Flask(__name__)
 
 config = {
   'user': 'abyan',
-  'password': 'password',
+  'password': '7kxtaHVie4MEpufAp7LU',
   'host': 'terraform-20240422044038018300000001.c4avp797njw5.ap-southeast-1.rds.amazonaws.com',
   'database': 'superservice',
+  'port': '3306',
   'raise_on_warnings': True
 }
 
 @app.route('/healthcheck')
 def healthcheck():
-    cnx = mysql.connector.connect(**config)
+    try:
+        cnx = mysql.connector.connect(**config)
+    except mysql.connector.Error as err:
+        app.logger.error("Could not connect to DB: %s", err)
+        return "Could not connect to DB"
     if cnx.is_connected():
         return "OK"
     cnx.close()
@@ -22,16 +28,6 @@ def healthcheck():
 @app.route('/')
 def index():
     result = []
-    cnx = mysql.connector.connect(**config)
-    if cnx and cnx.is_connected():
-        with cnx.cursor() as cursor:
-            cursor.execute("SELECT * FROM service LIMIT 5")
-            rows = cursor.fetchall()
-            for row in rows:
-                result.append(row)
-        cnx.close()
-    else:
-        result = "Could not connect to DB"
     return render_template('index.html', result=result)
 
 
@@ -42,3 +38,6 @@ def polls():
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=8080)
+    gunicorn_logger = logging.getLogger('gunicorn.warn')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
