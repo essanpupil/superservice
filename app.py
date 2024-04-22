79e879525de48f44a6f4a8823ae7a110d5dce541
Ikhsan N. Rosyidin <ikhsan.rosyidin@traveloka.com>
2024-04-22 13:35:09 +0700

    Connect to database
    Signed-off-by: Ikhsan N. Rosyidin <ikhsan.rosyidin@traveloka.com>

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
 
+
 @app.route('/healthcheck')
 def healthcheck():
-    return "OK"
+    cnx = mysql.connector.connect(**config)
+    if cnx.is_connected():
+        return "OK"
+    cnx.close()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    result = []
+    cnx = mysql.connector.connect(**config)
+    if cnx and cnx.is_connected():
+        with cnx.cursor() as cursor:
+            cursor.execute("SELECT * FROM service LIMIT 5")
+            rows = cursor.fetchall()
+            for row in rows:
+                result.append(row)
+        cnx.close()
+    else:
+        result = "Could not connect to DB"
+    return render_template('index.html', result=result)
 
 
 @app.route('/polls')
