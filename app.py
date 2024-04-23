43d2f8bbc60fdfaed6c06ddf1316539d91531f67
Ikhsan N. Rosyidin <ikhsan.rosyidin@traveloka.com>
2024-04-23 18:00:48 +0700

    Use task definition env var for database user passwd

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
+import os
+
 from flask import Flask, render_template
 import mysql.connector
 import logging
+import urllib
+
+
+config = {
+    'user': os.environ.get("DB_USER"),
+    'password': os.environ.get("DB_PASSWORD"),
+    'host': 'superservice-db.c4avp797njw5.ap-southeast-1.rds.amazonaws.com',
+    'database': 'superservice',
+    'port': '3306',
+    'raise_on_warnings': True
+}
+
 
 app = Flask(__name__)
 if __name__ != '__main__':
@@ -8,14 +22,6 @@ if __name__ != '__main__':
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
 
-config = {
-  'user': 'abyan',
-  'password': '7kxtaHVie4MEpufAp7LU',
-  'host': 'terraform-20240422044038018300000001.c4avp797njw5.ap-southeast-1.rds.amazonaws.com',
-  'database': 'superservice',
-  'port': '3306',
-  'raise_on_warnings': True
-}
 
 @app.route('/healthcheck')
 def healthcheck():
@@ -31,7 +37,60 @@ def healthcheck():
 @app.route('/')
 def index():
     result = []
+    try:
+        cnx = mysql.connector.connect(**config)
+    except Exception as err:
+        app.logger.error("Could not connect to DB: %s", err)
+        result = "Could not connect to DB"
+        return render_template('index.html', result=result)
+
+    if cnx and cnx.is_connected():
+        with cnx.cursor() as cursor:
+            cursor.execute("SELECT * FROM service LIMIT 5")
+            rows = cursor.fetchall()
+            for row in rows:
+                result.append(row)
+        cnx.close()
+    else:
+        result = "Could not connect to DB"
+
+    try:
+        question = urllib.request.urlopen("http://internal-polls-1382146748.ap-southeast-1.elb.amazonaws.com:8000").read()
+    except Exception as err:
+        app.logger.error("Could not connect to Polls: %s", err)
+        question = "Could not connect to Polls: {}".format(err)
+
+    return render_template('index.html', result=result, question=question)
+
+
+@app.route('/create-service-table')
+def create_service():
+    result = []
+    try:
+        cnx = mysql.connector.connect(**config)
+    except Exception as err:
+        app.logger.error("Could not connect to DB: %s", err)
+        result = "Could not connect to DB"
+        return render_template('index.html', result=result)
+
+    if cnx and cnx.is_connected():
+        with cnx.cursor() as cursor:
+            cursor.execute("CREATE TABLE service (name VARCHAR(255), endpoint VARCHAR(255))")
+        cnx.close()
+    else:
+        result = "Could not connect to DB"
+
     return render_template('index.html', result=result)
+
+@app.route('/create-question-table')
+def create_question():
+    try:
+        question = urllib.request.urlopen("http://internal-polls-1382146748.ap-southeast-1.elb.amazonaws.com:8000/create-question-table").read()
+    except Exception as err:
+        app.logger.error("Could not connect to Polls: %s", err)
+        question = "Could not connect to Polls: {}".format(err)
+
+    return render_template('index.html', result=question)
 
 
 @app.route('/polls')
