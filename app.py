b5c3fd9f17317709f23e02c0bb413e8053b1d940
Ikhsan N. Rosyidin <ikhsan.rosyidin@traveloka.com>
2024-04-24 17:59:23 +0700

    Use SQLAlchemy for database

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 import os
 
 from flask import Flask, render_template
-import mysql.connector
 import logging
 import urllib
-
-
-config = {
-    'user': os.environ.get("DB_USER"),
-    'password': os.environ.get("DB_PASSWORD"),
-    'host': 'superservice-db.c4avp797njw5.ap-southeast-1.rds.amazonaws.com',
-    'database': 'superservice',
-    'port': '3306',
-    'raise_on_warnings': True
-}
-
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://{uname}:{passwd}@superservice-db.c4avp797njw5.ap-southeast-1.rds.amazonaws.com/superservice".format(uname=os.environ.get("DB_USER"), passwd=os.environ.get("DB_PASSWORD"))
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+class Service(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    endpoint: Mapped[str]
+
+
 if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
@@ -25,35 +27,13 @@ if __name__ != '__main__':
 
 @app.route('/healthcheck')
 def healthcheck():
-    try:
-        cnx = mysql.connector.connect(**config)
-    except mysql.connector.Error as err:
-        app.logger.error("Could not connect to DB: %s", err)
-        return "Could not connect to DB"
-    if cnx.is_connected():
-        return "OK"
+    db.session.execute(db.select(Service).order_by(Service.name)).scalars()
+    return "OK", 200
 
 
 @app.route('/')
 def index():
-    result = []
-    try:
-        cnx = mysql.connector.connect(**config)
-    except Exception as err:
-        app.logger.error("Could not connect to DB: %s", err)
-        result = "Could not connect to DB"
-        return render_template('index.html', result=result)
-
-    if cnx and cnx.is_connected():
-        with cnx.cursor() as cursor:
-            cursor.execute("SELECT * FROM service LIMIT 5")
-            rows = cursor.fetchall()
-            for row in rows:
-                result.append(row)
-        cnx.close()
-    else:
-        result = "Could not connect to DB"
-
+    result = db.session.execute(db.select(Service).order_by(Service.name)).scalars()
     try:
         question = urllib.request.urlopen("http://internal-polls-1382146748.ap-southeast-1.elb.amazonaws.com:8000").read()
     except Exception as err:
@@ -65,22 +45,9 @@ def index():
 
 @app.route('/create-service-table')
 def create_service():
-    result = []
-    try:
-        cnx = mysql.connector.connect(**config)
-    except Exception as err:
-        app.logger.error("Could not connect to DB: %s", err)
-        result = "Could not connect to DB"
-        return render_template('index.html', result=result)
-
-    if cnx and cnx.is_connected():
-        with cnx.cursor() as cursor:
-            cursor.execute("CREATE TABLE service (name VARCHAR(255), endpoint VARCHAR(255))")
-        cnx.close()
-    else:
-        result = "Could not connect to DB"
-
+    result = db.session.execute(db.select(Service).order_by(Service.name)).scalars()
     return render_template('index.html', result=result)
+
 
 @app.route('/create-question-table')
 def create_question():
